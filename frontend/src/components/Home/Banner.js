2d052966ea512a745b887b9662cd6646eb8e2512
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2023-01-30 18:44:56 +0000

    implement search mechanism

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,23 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  
+  const onSearchChange = (e) => {
+    props.onSearchFilter(e.target.value, (page) => agent.Items.byTitle(e.target.value), agent.Items.byTitle(e.target.value))
+  }
+  
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
+          <span id="get-part">get </span>
+          <form>
+            <input type="text" id="search-box" style={{width: "250px"}} name="term" onChange={onSearchChange}/>
+          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
